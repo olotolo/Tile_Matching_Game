d8e765a2bae4846953929e794bc211be16638dc2
olotolo <olotolo@web.de>
2022-12-30 00:28:47 +0100

    Fixed Bugs and created more

--- Tile_Matching_Game/Assets/Code/GemController.cs
+++ Tile_Matching_Game/Assets/Code/GemController.cs
@@ -13,7 +13,6 @@ public class GemController : MonoBehaviour
     public Vector3 FirstObj;
     public Vector3 SecondObj;
 
-    private int _id;
 
     public bool active = false;
     public GameObject selected;
@@ -44,7 +43,6 @@ public class GemController : MonoBehaviour
         gem.name = "Gem " + x + " " + y;
         gem.GetComponent<Tile>().x = x;
         gem.GetComponent<Tile>().y = y;
-        gem.GetComponent<Tile>().id = _id++;
         gems[x].Add(gem);
     }
 
@@ -54,11 +52,14 @@ public class GemController : MonoBehaviour
         GameObject last = null;
         GameObject current = null;
         int streak = 0;
-        List<List<GameObject>> found = new List<List< GameObject>>();
+        List<List<GameObject>> found = new List<List<GameObject>>();
         List<GameObject> list = new List<GameObject>();
+        // Vertical
         for(int i = 0; i < gems.Count; i++)
         {
             last = gems[i][0];
+            last.GetComponent<Tile>().x = i;
+            last.GetComponent<Tile>().y = 0;
             current = null;
             list.Clear();
             streak = 0;
@@ -66,6 +67,8 @@ public class GemController : MonoBehaviour
             {
                 if (current != null) last = current;
                 current = gems[i][j];
+                current.GetComponent<Tile>().x = i;
+                current.GetComponent<Tile>().y = j;
                 if (last.GetComponent<Tile>().Type == current.GetComponent<Tile>().Type) 
                 {
                     streak++;
@@ -81,34 +84,76 @@ public class GemController : MonoBehaviour
                 list.Add(current);
             }
             if (streak >= 3) found.Add(new List<GameObject>(list));
-
-
         }
 
+        // Horizontal
+        for(int i = 0; i < gems[0].Count; i++)
+        {
+            last = gems[0][i];
+            current = null;
+            list.Clear();
+            streak = 0;
+            for(int j = 0; j < gems[i].Count; j++)
+            {
+                if (current != null) last = current;
+                current = gems[j][i];
+                if(last.GetComponent<Tile>().Type == current.GetComponent<Tile>().Type)
+                {
+                    streak++;
+                } else
+                {
+                    if(streak >= 3)
+                    {
+                        found.Add(new List<GameObject>(list));
+                        
+                    }
+                    streak = 1;
+                    list.Clear();
+                }
+                list.Add(current);
+            }
+            if (streak >= 3) found.Add(new List<GameObject>(list));
+        }
+
+
+
+        
         for(int i = 0; i < found.Count; i++)
         {
             for(int j = 0; j < found[i].Count; j++)
-
             {
-                Debug.Log(gems[found[i][j].GetComponent<Tile>().x][found[i][j].GetComponent<Tile>().y].name);
-                gems[found[i][j].GetComponent<Tile>().x][found[i][j].GetComponent<Tile>().y].name = "a";
+                
+
+                gems[found[i][j].GetComponent<Tile>().x][found[i][j].GetComponent<Tile>().y].name = "deleted";
                 Destroy(gems[found[i][j].GetComponent<Tile>().x][found[i][j].GetComponent<Tile>().y]);
+                Debug.Log("DESTROYED");
             }
+        }
+
+
+        int indexi = 0;
+        int indexj = 0;
+
+        while(indexi < gems.Count)
+        {
+            indexj = 0;
+            while(indexj < gems[indexi].Count)
+            {
+                Debug.Log("run");
+                if (gems[indexi][indexj].name == "deleted")
+                {
+                    Debug.Log("DELETED");
+                    gems[indexi].RemoveAt(indexj);
+                    continue;
+                }
+                indexj++;
+            }
+            indexi++;
         }
         
-        for(int i = 0; i < gems.Count; i++){
-            for(int j = 0; j < gems[i].Count; j++)
-            {
-                if (gems[i][j].name == "a")
-                {
-                    gems[i].RemoveAt(j);
-                    j--;
-                }
-            }
-        }
-       
+        found.Clear();
+        list.Clear();
 
-        //gems[found[i][j].GetComponent<Tile>().x].RemoveAt(found[i][j].GetComponent<Tile>().y);
         
     }
 
@@ -119,12 +164,9 @@ public class GemController : MonoBehaviour
     {
         for(int i = 0; i < width; i++)
         {
-            int y = 6;
             while (gems[i].Count < height)
             {
-                InstantiateGem(i, y);
-                y++;
-                
+                InstantiateGem(i, gems[i].Count);
             }
 
 
@@ -144,27 +186,39 @@ public class GemController : MonoBehaviour
                 {
                     //Select first
                     selected = hit.collider.gameObject;
-                    hit.collider.gameObject.GetComponent<SpriteRenderer>().color = Color.black;
                     active = true;
 
                 } else
                 {
                     //Select second and swap
-                    int holder = selected.gameObject.GetComponent<Tile>().Type;
+                    GameObject holder = selected.gameObject;
                     selected.gameObject.GetComponent<Tile>().Type = hit.collider.gameObject.GetComponent<Tile>().Type;
-                    hit.collider.GetComponent<Tile>().Type = holder;
+                    hit.collider.GetComponent<Tile>().Type = holder.GetComponent<Tile>().Type;
+
+                    int layer1 = selected.GetComponent<Tile>().Type;
+                    int layer2 = hit.collider.gameObject.GetComponent<Tile>().Type;
+
+                    selected.gameObject.GetComponent<SpriteRenderer>().sprite = gemPrefab[layer2].GetComponent<SpriteRenderer>().sprite;
+                    hit.collider.gameObject.GetComponent<SpriteRenderer>().sprite = gemPrefab[layer1].GetComponent<SpriteRenderer>().sprite;
+                    active = false;
+
+
+                    /*
                     FindDups();
                     Refill();
+                    */
 
-
-                    active = false;
                 }
 
 
-                //FindDups();
-                //Refill();
 
             }
+
+        }
+        if(Input.GetMouseButtonDown(1))
+        {
+            FindDups();
+            Refill();
         }
         
     }
